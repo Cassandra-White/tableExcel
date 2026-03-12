a7bd4f5d933270f72c6e09fe87c33b526641edbb
KRISSANE Alexandre <49199655+Cassandra-White@users.noreply.github.com>
2026-03-12 14:34:27 +0100

    Update InstallationPaquetServeurFichier.sh

--- InstallationPaquetServeurFichier.sh
+++ InstallationPaquetServeurFichier.sh
@@ -1,16 +1,10 @@
-# -------- 2. Paquets --------
-# samba + winbind + libnss/libpam : integration SMB dans AD billu.local
-# krb5-user : authentification Kerberos (requis pour la jonction)
-# acl attr : ACL POSIX etendues (setfacl / getfacl)
-# lvm2 : gestion volumes logiques (Obj.3)
-# mdadm : RAID logiciel (Obj.2)
-# rsync : sauvegarde incrementielle (Obj.4)
+# -------- 2. Paquets -- Debian 13 Trixie (Samba 4.20+) --------
+# samba-common-bin : inclut la commande 'net ads join'
+# winbind + libnss-winbind + libpam-winbind : integration AD
+# krb5-user (MIT Kerberos) : authentification Kerberos
+# acl attr : ACL POSIX etendues
+# lvm2 mdadm rsync : stockage et sauvegarde
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -y     samba samba-common-bin winbind     libnss-winbind libpam-winbind     krb5-user smbclient     acl attr     lvm2 mdadm     rsync net-tools curl
 
-apt-get update
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    samba winbind libnss-winbind libpam-winbind \
-    krb5-user smbclient \
-    acl attr \
-    lvm2 mdadm \
-    rsync chrony net-tools curl
+# -------- 3. LVM
