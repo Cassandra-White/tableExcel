0929e0ea2a198c90448a5c83b4784eda747cce7c
KRISSANE Alexandre <49199655+Cassandra-White@users.noreply.github.com>
2026-03-12 13:56:42 +0100

    Create InstallationPaquetServeurFichier.sh

---
+++ InstallationPaquetServeurFichier.sh
@@ -0,0 +1,14 @@
+# -------- 2. Paquets --------
+# samba + winbind + libnss/libpam : integration SMB dans AD billu.local
+# krb5-user : authentification Kerberos (requis pour la jonction)
+# acl attr : ACL POSIX etendues (setfacl / getfacl)
+# lvm2 : gestion volumes logiques (Obj.3)
+# mdadm : RAID logiciel (Obj.2)
+# rsync : sauvegarde incrementielle (Obj.4)
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    samba winbind libnss-winbind libpam-winbind \
+    krb5-user smbclient \
+    acl attr \
+    lvm2 mdadm \
+    rsync ntp net-tools curl
