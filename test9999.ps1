a7a16ca957f926d90083db9615b910ff32047cc3
KRISSANE Alexandre <49199655+Cassandra-White@users.noreply.github.com>
2026-03-15 14:33:31 +0100

    Update test9999.ps1

--- test9999.ps1
+++ test9999.ps1
@@ -39,4 +39,40 @@ Set-GPRegistryValue -Name "BillU-LecteursReseau" `
     -Value "powershell.exe -ExecutionPolicy Bypass -NonInteractive -WindowStyle Hidden -File `"\\billu.local\NETLOGON\Map-Drives-BillU.ps1`""
 
 
-    
+
+
+
+
+
+
+# Sur DC1 -- corriger le chemin dans Files.xml
+
+$g    = Get-GPO -Name "BillU-Bureau"
+$guid = $g.Id.ToString("B")
+$xml  = "C:\Windows\SYSVOL\sysvol\billu.local\Policies\$guid\User\Preferences\Files\Files.xml"
+
+# Verifier le contenu actuel
+Get-Content $xml | Select-String "fromPath"
+
+
+
+# Remplacer le chemin dans le XML
+(Get-Content $xml) -replace 'fromPath="[^"]*"', 'fromPath="\\billu.local\NETLOGON\Reconnecter-Lecteurs.bat"' |
+    Set-Content $xml -Encoding UTF8
+
+# Verifier la correction
+Get-Content $xml | Select-String "fromPath"
+
+# Incrementer la version GPO pour forcer reapplication
+$gptIni  = "C:\Windows\SYSVOL\sysvol\billu.local\Policies\$guid\GPT.INI"
+$content = Get-Content $gptIni
+$content = $content | ForEach-Object {
+    if ($_ -match '^Version=(\d+)') { "Version=$([int]$matches[1] + 1)" } else { $_ }
+}
+$content | Set-Content $gptIni
+Write-Host "GPT.INI mis a jour"
+```
+
+Puis sur le client :
+```
+gpupdate /force
