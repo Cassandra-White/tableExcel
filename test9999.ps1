bf8bd315673df7ca1d234e9c7ab501d0af25986f
KRISSANE Alexandre <49199655+Cassandra-White@users.noreply.github.com>
2026-03-15 13:41:51 +0100

    Update test9999.ps1

--- test9999.ps1
+++ test9999.ps1
@@ -1,3 +1,10 @@
 Copy-Item ".\Map-Drives-BillU.ps1" "C:\Windows\SYSVOL\sysvol\billu.local\scripts\" -Force
 
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "BillU-Shortcut" -EA SilentlyContinue
+
+
+$g = Get-GPO -Name "BillU-Bureau"
+$guid = $g.Id.ToString("B")
+$xml = "C:\Windows\SYSVOL\sysvol\billu.local\Policies\$guid\User\Preferences\Files\Files.xml"
+Test-Path $xml   # doit retourner True
+Get-Content $xml # doit afficher le XML avec le chemin du bat
